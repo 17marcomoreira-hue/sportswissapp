4640931444437949823973cba031af53130c7df4
17marcomoreira-hue <17marcomoreira@gmail.com>
2026-02-06 20:58:06 +0100

    Update Firebase configuration with real credentials

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,12 +1,13 @@
 // js/firebase-config.js
 // Colle ici l'objet firebaseConfig fourni par Firebase Console.
+
 export const firebaseConfig = {
-  apiKey: "REMPLACE_MOI",
-  authDomain: "REMPLACE_MOI",
-  projectId: "REMPLACE_MOI",
-  storageBucket: "REMPLACE_MOI",
-  messagingSenderId: "REMPLACE_MOI",
-  appId: "REMPLACE_MOI"
+  apiKey: "AIzaSyAx9FPFnhlWow3nOV6Aw5Qqy4oEjsad_MU",
+  authDomain: "sportswissapp-login.firebaseapp.com",
+  projectId: "sportswissapp-login",
+  storageBucket: "sportswissapp-login.firebasestorage.app",
+  messagingSenderId: "416514820592",
+  appId: "1:416514820592:web:a653b4e28b6215dd813535"
 };
 
 // Admin(s) autorisé(s) à gérer les clés sur /admin.html
